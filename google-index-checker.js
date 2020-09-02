cd2668029445081325d19bd8773e639ac721203b
Alvaro <alvaro@builtvisible.com>
2020-09-02 11:31:49 +0100

    Batch parallel promises

--- google-index-checker.js
+++ google-index-checker.js
@@ -2,27 +2,27 @@
 const ck = require('chalk') // Terminal and string styling
 const fs = require('fs') // Node file system module
 const csv = require('csvtojson') // Convert csv to json module
-const axios = require('./lib/axios-queue')(5) // Axios concurrent limit set to 5
+const axios = require('axios') // Axios client
 const { requestUrl, compareUrl } = require('./lib/url-encoder') // Encoding functions
-const { timer } = require('./lib/timer') // Encoding functions
+const { timer } = require('./lib/timer') // Timer function
 // Settings
 const start = Date.now() // Date counter to check duration of script
 const site = 'https://www.google.com/search?q=' // Google search query
 const urls = './urls.csv' // File containing all the urls to check
-const apiUrl = 'http://api.scraperapi.com/?api_key=' // Scrapeapi url
-const { apiKey } = require('./APIKEY') // Scrapeapi key
+const apiUrl = 'http://api.scraperapi.com/?api_key=' // ScraperAPI url
+const { apiKey } = require('./APIKEY') // ScraperAPI key
 
 ;(() => {
   const app = {
     totalUrls: 0,
     notIndexCounter: 0,
 
-    // Check if file exist and count urls, if it does not exists, exit with message
+    // Check if file exist and count number of urls, if it does not exists, exit with message
     init: async () => {
       if (fs.existsSync(urls)) {
         // Store the total amount of urls
         const data = await csv({ noheader: true, output: 'line' }).fromFile(urls)
-        app.totalUrls = data.filter(url => url.length !== 0).length
+        app.totalUrls = data.filter((url) => url.length !== 0).length
         app.getUrls()
       } else {
         console.log(ck.yellow('No urls.csv file found.'))
@@ -30,35 +30,67 @@ const { apiKey } = require('./APIKEY') // Scrapeapi key
       }
     },
 
-    // Initial request converts the `urls` list into an array and runs the request
+    // Initial request converts the `urls` list into an array and executes parallel http request
     getUrls: async () => {
       // Counter to keep track of current number being checked
       app.num = 1
+
       // Convert the csv to an array from `urls`
       let data = await csv({ noheader: true, output: 'line' }).fromFile(urls)
+
       // Filter csv empty lines
-      data = data.filter(url => url.length !== 0)
-      // Store promises for concurrent requests
-      const promises = data.map((url, index) => {
-        return new Promise(resolve => {
-          setTimeout(
-            resolve,
-            index * 10,
-            app.runRequest(url, data.length).catch(err => console.log(err))
-          )
-        })
-      })
-      // When resolved check for errors
-      await Promise.all(promises).then(() => app.checkErrors())
+      data = data.filter((url) => url.length !== 0)
+
+      // Parallel batch promises for concurrent requests
+
+      // Get concurrent max number based on API key
+      const concurrent = await axios(`http://api.scraperapi.com/account?api_key=${apiKey}`)
+
+      // Create clone obj with indexes
+      const argCopy = [].concat(data.map((val, index) => ({ val, index })))
+
+      // Create array with an index for each item of its length
+      const result = new Array(data.length)
+
+      // Array with the max concurrent allow and fill each resolved promise
+      const promises = new Array(concurrent.data.concurrencyLimit).fill(Promise.resolve())
+
+      // Recursive http request
+      const chainNext = (promised) => {
+        // If there are items left run again
+        if (argCopy.length) {
+          // Take the first one away from the array and use that for the request
+          const arg = argCopy.shift()
+          // Execute the request
+          return promised.then(() => {
+            const promiseOperation = app.runRequest(arg.val, data.length).then((res) => {
+              // Fill the empty container with the resolved promise
+              result[arg.index] = res
+            })
+            // Re-run
+            return chainNext(promiseOperation)
+          })
+        }
+        // Return results
+        return promised
+      }
+
+      // When done check for errors
+      await Promise.all(promises.map(chainNext)).then(() => app.checkErrors())
     },
 
     // HTTP request async promise
     runRequest: async (url, len) => {
       // Make requests with encoded URL through axios with header options and write it to results
       try {
-        const request = requestUrl(url) // URL encoded for request
-        const compare = compareUrl(url, false) // URL encoded to check indexation
-        const utfEncoded = compareUrl(url, true) // URL encoded for discrepancies
+        // URL encoded for request
+        const request = requestUrl(url)
+
+        // URL encoded to check indexation
+        const compare = compareUrl(url, false)
+
+        // URL encoded for discrepancies
+        const utfEncoded = compareUrl(url, true)
 
         // HTTPS request using axios, scraperapi, google and the enconded url
         const res = await axios(`${apiUrl}${apiKey}&url=${site}${request}`)
@@ -75,12 +107,15 @@ const { apiKey } = require('./APIKEY') // Scrapeapi key
             )}`
           )
         )
+
         // Create append streamclear
         const stream = fs.createWriteStream('./results.csv', { flags: 'a', encoding: 'utf8' })
+
         // Append evaluation from response to file
         stream.write(`${url}, ${indexation}\n`)
+
+        // End stream to avoid accumulation
         stream.end()
-        //
       } catch (err) {
         // When there is an error write it to errors.csv or exceptions.csv
         let msg = ''
@@ -102,11 +137,15 @@ const { apiKey } = require('./APIKEY') // Scrapeapi key
         console.log(ck.yellow(`Checking: ${app.num++}/${len} ${url} ${ck.red(msg)}`))
         // Create append stream
         const errorStream = fs.createWriteStream(
+          // If there are invalid urls it will go to the excepctions file
           msg === 'Exception' ? './exceptions.csv' : './errors.csv',
           { flags: 'a', encoding: 'utf8' }
         )
+
         // Append to file
         errorStream.write(`${url}\n`)
+
+        // End stream to avoid accumulation
         errorStream.end()
       }
     },
@@ -127,12 +166,12 @@ const { apiKey } = require('./APIKEY') // Scrapeapi key
       return matchURL
     },
 
-    // Remove url.csv rename it to errors to urls.csv and run request again
+    // Remove urls.csv, rename errors.csv to urls.csv and run request again
     checkErrors: () => {
-      // If there's no erros.csv finish and log duration
-      fs.rename('./errors.csv', urls, err => {
+      // If there's no errors.csv, finish and log duration
+      fs.rename('./errors.csv', urls, (err) => {
         if (err) {
-          fs.unlink(urls, err => {
+          fs.unlink(urls, (err) => {
             if (err) console.log('Error deleting url.csv')
           })
           console.log(
