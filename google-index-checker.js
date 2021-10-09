58ab05730fe530d7963c8388cd6bdb5f058321d8
alvaro-naves <a.fernandez.naves@gmail.com>
2021-10-09 16:24:28 +0100

    Updated request using pool and added device parameter

--- google-index-checker.js
+++ google-index-checker.js
@@ -1,37 +1,47 @@
 // Required Modules
 import 'dotenv/config'
 import chalk from 'chalk' // Terminal and string styling
-import { createWriteStream } from 'fs' // Node file system module
+import axios from 'axios' // Axios client
+import { createWriteStream, writeFileSync } from 'fs' // Node file system module
 import { access } from 'fs/promises' // Promises Node file system module
 import { parseCSV } from './lib/parser.js' // Convert csv to json module
-import axios from 'axios' // Axios client
-import { requestUrl, compareUrl } from './lib/url-encoder.js' // Encoding functions
+import { googlelify, encodeURL } from './lib/url-encoder.js' // Encoding functions
 import { timer } from './lib/timer.js' // Timer function
+import { poolRequest } from './lib/poolRequest.js'
+import sanitizeHtml from 'sanitize-html'
+
 // Settings
 const { yellow, cyan, white, red, green } = chalk
 const start = Date.now() // Date counter to check duration of script
 const site = 'https://www.google.com/search?q=' // Google search query
 const urlsFile = './urls.csv' // File containing all the urls to check
 const apiUrl = 'http://api.scraperapi.com/?api_key=' // ScraperAPI url
+const params = '&device_type=desktop'
 const apiKey = process.env.SCRAPERAPI_KEY
 
-let totalUrls = 0
-let notIndexCounter = 0
-let instances = 0
-let urls = []
-// Check if file exist and count number of urls, if it does not exists, exit with message
+let count = 1
+let notIndexedCounter = 0
+
+// Collect URLS, get Concurrent max max a run request in pool
 ;(async () => {
-  try {
-    await access(urlsFile)
-    urls = await parseCSV(urlsFile)
-    totalUrls = urls.length
-    batchRequest()
-  } catch {
-    console.log(yellow('No urls.csv file found.'))
-    process.exit()
-  }
+  urls = await getUrls()
+  const concurrent = await getConcurrent()
+  await poolRequest(concurrent, [...urls], runRequest, 'timeout')
+  finalMessage(urls.length)
 })()
 
+// Gather URLS from file
+async function getUrls() {
+  try {
+    await access(urlsFile)
+    return await parseCSV(urlsFile)
+  } catch {
+    console.log(yellow('No urls.csv file found.'))
+    process.exit(1)
+  }
+}
+
+// Connect to API to get allowed number of concurrent requests
 async function getConcurrent() {
   try {
     const { data } = await axios(`http://api.scraperapi.com/account?api_key=${apiKey}`)
@@ -42,63 +52,36 @@ async function getConcurrent() {
         `${error.response.status} - Incorrect or missing API key please check your APIKEY.js file and make sure it includes a correct API key from https://www.scraperapi.com/`
       )
     } else {
-      console.error('There is a problem connecting to Scraperapi')
-      process.exit(1)
+      console.error('There is a problem connecting to Scraperapi, please try again later')
+      process.exit()
     }
   }
 }
 
-// Batch request with maximun according to account
-async function batchRequest() {
-  console.log(green('Requesting...'))
-  instances = 1
-
-  const data = [...urls]
-  const concurrent = await getConcurrent()
-
-  while (data.length) {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-
-    const batch = data.splice(0, concurrent).map((url) => runRequest(url, urls.length))
-
-    const results = await Promise.allSettled(batch)
-
-    for (const { status, reason } of results) {
-      if (status === 'rejected') data.push(reason)
-    }
-  }
-
-  finalMessage()
-}
-
-// HTTP request async promise
-async function runRequest(url, len) {
-  // Make requests with encoded URL through axios with header options and write it to results
+// HTTP request async
+async function runRequest(url) {
   try {
-    // URL encoded for request
-    const request = requestUrl(url)
-    // URL encoded to check indexation
-    const compare = compareUrl(url, false)
-    // URL encoded for discrepancies
-    const utfEncoded = compareUrl(url, true)
-    // HTTPS request using axios, scraperapi, google and the enconded url
-    const res = await axios(`${apiUrl}${apiKey}&url=${site}${request}`)
-    const indexation = matchResponse(url, res.data, compare, utfEncoded)
-      ? 'Indexed'
-      : 'Not Indexed'
-    // Print to terminal each url, its number and status code
-    console.log(
-      cyan(
-        `Checking: ${instances++}/${len} ${url} ${green.bold(res.status)} ${white.bold(
-          indexation
-        )}`
-      )
+    // Prepare url to search like google does
+    const requestUrl = googlelify(url)
+
+    // HTTP request using axios, scraperapi, google and the enconded url
+    const { data, status } = await axios(
+      `${apiUrl}${apiKey}&url=${site}${requestUrl}${params}`
     )
-    // Create append streamclear
-    const stream = createWriteStream('./results.csv', {
-      flags: 'a',
-      encoding: 'utf8'
-    })
+
+    // Check if it matches google search results
+    const indexation = matchResponse(url, data)
+
+    // Print to terminal each url, its number and status code
+    const counter = `${count++}/${urls.length}`
+    const statusPrint = green.bold(status)
+    const indexPrint = white.bold(indexation)
+
+    console.log(cyan(`Checking: ${counter} ${url} ${statusPrint} ${indexPrint}`))
+
+    // Create, append and clear stream
+    const stream = createWriteStream('./results.csv', 'utf8')
+
     // Append evaluation from response to file
     stream.write(`${url}, ${indexation}\n`)
     // End stream to avoid accumulation
@@ -107,38 +90,51 @@ async function runRequest(url, len) {
     // Request made and server responded
     const status = error.response ? error.response.status : 500
 
-    // Log with different color to highlight the error
-    console.error(yellow(`Error: ${url} ${red(status)} ${green('Repeating')}`))
+    if (status === 429) {
+      console.error('Too many request, something went wrong check with SpaperAPI')
+      process.exit(1)
+    }
 
-    throw url
+    // Log with different color to highlight the error
+    console.error(yellow(`Error: ${url} ${red(status)} ${green('Re-trying')}`))
+
+    throw {
+      name: 'timeout',
+      error: url
+    }
   }
 }
 
 // Compare url against google response url
-function matchResponse(url, res, compare, utfEncoded) {
-  let matchURL = false
+function matchResponse(url, res) {
+  // Look for a tags with href attribute only
+  const content = sanitizeHtml(res, {
+    allowedTags: ['a'],
+    allowedAttributes: { a: ['href'] }
+  })
 
-  if (url.includes(`'`)) {
-    matchURL = res.includes(`href="${compare}"`) | res.includes(`href="${utfEncoded}"`)
+  // Set not index to start
+  let indexResult = 'Not Indexed'
+
+  // If the encoded version of the URL is on google
+  if (content.includes(`href="${encodeURL(url)}"`)) {
+    indexResult = 'Indexed'
   } else {
-    matchURL = res.includes(`href="${compare}"`)
+    notIndexedCounter += 1
   }
 
-  // Counter foreach not index
-  if (!matchURL) notIndexCounter += 1
-
-  return matchURL
+  return indexResult
 }
 
-function finalMessage() {
+function finalMessage(totalUrls) {
   console.log(
     `\n${totalUrls} URLS, results.csv file successfully written in ${timer(
       Date.now() - start
     )}\n`
   )
   console.log(
-    `${green.bold(`Indexed: ` + (totalUrls - notIndexCounter))}\n${red.bold(
-      `Not indexed: ` + notIndexCounter + `\n`
+    `${green.bold(`Indexed: ` + (totalUrls - notIndexedCounter))}\n${red.bold(
+      `Not indexed: ` + notIndexedCounter + `\n`
     )}`
   )
 }
